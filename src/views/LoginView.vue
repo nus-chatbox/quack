3583567f38a526681df000f58abb15053563c9c1
Si Kai <sikai.ngsk@gmail.com>
2017-09-18 02:43:56 +0800

    basic front end structure with Quasar framework

---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <q-layout>
+      <div class="layout-padding text-center">
+        <img class="responsive" src="../assets/logo.png" />
+        <p class="caption"><h2><strong>Quack</strong></h2></p>
+        <p class="caption"><h5>Make chats social again.</h5></p>
+      </div>
+      <q-btn class="fb-button" icon-right="lock" big v-ripple color="#3B5998">
+        Sign in with Facebook
+      </q-btn>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+import { Ripple, QBtn, QLayout } from 'quasar-framework';
+
+export default {
+  directives: {
+    Ripple,
+  },
+  components: {
+    QBtn,
+    QLayout,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.fb-button
+  background-color: #3B5998
+</style>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <q-layout>
+      <div class="layout-padding text-center">
+        <p class="caption"><h2><strong>Oops! Looks like you lost your way.</strong></h2></p>
+      </div>
+      <router-link to="/">
+        <q-btn icon-right="child care" big v-ripple color="primary">
+          Back to Quacking
+        </q-btn>
+      </router-link>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+import { Ripple, QBtn, QLayout } from 'quasar-framework';
+
+export default {
+  directives: {
+    Ripple,
+  },
+  components: {
+    QBtn,
+    QLayout,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+</style>
