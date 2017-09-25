174cd0909ec7c3157744a5dfedc70106d39d2e96
Si Kai <sikai.ngsk@gmail.com>
2017-09-25 12:54:48 +0800

    added toggle and profile picture to side-nav

--- src/views/QuackAppView.vue
+++ src/views/QuackAppView.vue
@@ -10,8 +10,21 @@
         <q-btn flat icon="settings" @click="">
         </q-btn>
       </q-toolbar>
-      <q-scroll-area slot="left">
-      </q-scroll-area>
+      <q-list slot="left" no-border class="bg-light left-nav">
+        <div class="row flex-center bg-white" style="height: 100px;">
+          <img src="../assets/logo.png" style="height: 75px; width 75px;"/>
+          <div style="margin-left: 15px">Donald Duck</div>
+        </div>
+        <q-item-separator />
+        <q-list-header class="text-center">User Settings</q-list-header>
+        <div class="row flex-center">
+          <q-toggle v-model="isAnonymous" :left-label="true" label="Stay Anonymous" style="font-size:14px"/>
+        </div>
+        <q-item-separator />
+        <q-btn icon="exit to app" class="full-width" @click="logout()">
+          Log Out
+        </q-btn>
+      </q-list>
       <q-pull-to-refresh :handler="refreshChatGroupList">
         <chat-group-list></chat-group-list>
       </q-pull-to-refresh>
@@ -23,7 +36,7 @@
 </template>
 
 <script>
-import { Ripple, QBtn, QLayout, QToolbar, QToolbarTitle, QCard, QFixedPosition, QPullToRefresh, QScrollArea } from 'quasar-framework';
+import { Ripple, QBtn, QLayout, QToolbar, QToolbarTitle, QCard, QFixedPosition, QPullToRefresh, QList, QItemSeparator, QToggle, QListHeader } from 'quasar-framework';
 import ChatGroupList from '@/components/ChatGroupList';
 
 export default {
@@ -39,13 +52,27 @@ export default {
     ChatGroupList,
     QFixedPosition,
     QPullToRefresh,
-    QScrollArea
+    QList,
+    QItemSeparator,
+    QToggle,
+    QListHeader
+  },
+  data() {
+    return {
+      isAnonymous: false
+    };
   },
   methods: {
     refreshChatGroupList(done) {
       setTimeout(() => {
         done();
       }, 1000);
+    },
+    logout() {
+      this.$store.dispatch('logout')
+      .then(
+        this.$router.go('/')
+      );
     }
   }
 };
@@ -64,4 +91,8 @@ export default {
 .location-action-margin
   margin-bottom: 1.5rem
   margin-right: 1.5rem
+
+.left-nav
+  height: 100%
+  padding-top: 0px
 </style>
