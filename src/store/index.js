060dd104f3e26b954e09748cb2bd2e43a3e909a2
KnightNiwrem <merwinwee@gmail.com>
2017-09-27 20:16:42 +0800

    Bugfix and clean up

--- src/store/index.js
+++ src/store/index.js
@@ -9,43 +9,10 @@ Vue.use(Vuex);
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-console */
 /* eslint-disable no-shadow */
-const state = {
-  nearbyRooms: []
-};
-
-const mutations = {
-  setNearbyRooms(state, payload) {
-    state.nearbyRooms = payload;
-  }
-};
-
-const actions = {
-  getNearbyRooms({ commit, rootState }) {
-    return new Promise((resolve, reject) => {
-      fetch(`${window.apiUrl}/rooms`, {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `bearer ${rootState.login.jwtToken}`
-        }
-      })
-      .then(response => response.json())
-      .then((json) => {
-        commit('setNearbyRooms', json.rooms);
-        resolve(json.rooms);
-      })
-      .catch(err => reject(err));
-    });
-  }
-};
 
 export default new Vuex.Store({
   modules: {
     user: userModule,
     chat: chatModule
-  },
-  state,
-  mutations,
-  actions
+  }
 });
