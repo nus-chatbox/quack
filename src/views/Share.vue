923503bcc31b7f72c22820b30f26a4bb24d34456
tharain <won.jr@hotmail.com>
2017-09-27 19:37:10 +0800

    update icons

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -3,37 +3,49 @@
     <table style="cursor: pointer">
         <tr>
             <network network="facebook">
-            <i class="fa fa-fw fa-facebook"></i> Facebook
+            <i class="fa fa-fw fa-facebook"></i>
+            <img src="../assets/fbicon.png" style="width: 10%"/>
+            Facebook
             </network>
         </tr>
         <br>
         <tr>
             <network network="telegram">
-            <i class="fa fa-telegram"></i> Telegram
+            <i class="fa fa-telegram"></i>
+            <img src="../assets/tgicon.png" style="width: 10%"/>
+            Telegram
             </network>
         </tr>
         <br>
         <tr>
             <network network="googleplus">
-            <i class="fa fa-fw fa-google-plus"></i> Google +
+            <i class="fa fa-fw fa-google-plus"></i>
+            <img src="../assets/gicon.png" style="width: 10%"/>
+            Google +
             </network>
         </tr>
         <br>
         <tr>
             <network network="twitter">
-            <i class="fa fa-fw fa-twitter"></i> Twitter
+            <i class="fa fa-fw fa-twitter"></i>
+            <img src="../assets/ticon.png" style="width: 10%"/>
+            Twitter
             </network>
         </tr>
         <br>
         <tr>
             <network network="weibo">
-            <i class="fa fa-weibo"></i> Weibo
+            <i class="fa fa-weibo"></i>
+            <img src="../assets/wbicon.png" style="width: 10%"/>
+            Weibo
             </network>
         </tr>
         <br>
         <tr>
             <network network="whatsapp">
-            <i class="fa fa-fw fa-whatsapp"></i> Whatsapp
+            <i class="fa fa-fw fa-whatsapp"></i>
+            <img src="../assets/wicon.png" style="width: 10%"/>
+            Whatsapp
             </network>
         </tr>
         <br>
