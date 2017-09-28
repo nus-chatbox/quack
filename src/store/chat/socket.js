4aa1c4b4a276df3bd6f8a51536fa50ef28d8722c
KnightNiwrem <merwinwee@gmail.com>
2017-09-28 17:02:34 +0800

    Make notifications work with best effort scenario

--- src/store/chat/socket.js
+++ src/store/chat/socket.js
@@ -1,5 +1,31 @@
+import Notify from 'notifyjs';
+import quackLogo from '../../assets/logo.png';
+
 export default (store) => {
+  let permissionDenied = false;
+  let permissionGranted = false;
+
   window.apiSocket.on('message', (message) => {
+    if (!Notify.needsPermission) {
+      permissionGranted = true;
+    }
+
+    if (!permissionDenied && !permissionGranted && Notify.isSupported()) {
+      Notify.requestPermission(() => {
+        permissionGranted = true;
+      }, () => {
+        permissionDenied = true;
+      });
+    }
+
+    if (permissionGranted) {
+      const newNotification = new Notify('New Messagge', {
+        body: message.text,
+        icon: quackLogo
+      });
+      newNotification.show();
+    }
+
     store.commit('patchMessages', {
       messages: [message]
     });
