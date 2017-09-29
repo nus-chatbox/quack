a064a0e6ff4e64227ddca9f322399ba9809ae5fb
Si Kai <sikai.ngsk@gmail.com>
2017-09-29 20:25:22 +0800

    graphic when there are no groups

--- src/components/ChatGroupList.vue
+++ src/components/ChatGroupList.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <q-pull-to-refresh v-if="isRefreshing == false" :handler="refreshChatGroupList" class="group-list-vert">
-        <div v-for="room in nearbyRooms">
+        <div v-if="nearbyRooms.length === 0" class="layout-padding text-center no-rooms-padding">
+          <img src="/static/img/logo.png" class="responsive" style="max-width:12rem"/>
+          <div class="layout-padding">
+            <p>Oops! No one is quacking around you!</p>
+            <p><strong> Start a new quack? </strong></p>
+          </div>
+        </div>
+        <div v-else v-for="room in nearbyRooms">
           <q-transition appear enter="fadeIn" leave="fadeOut">
             <router-link :to="`/chat/${room.id}`">
               <chat-group :id="room.id" :name="room.title"></chat-group>
@@ -65,5 +72,6 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-
+.no-rooms-padding
+  padding-top: 3.5rem
 </style>
