a1b24257d1b344ed404aa778ecbf553e08a02863
Si Kai <sikai.ngsk@gmail.com>
2017-09-29 20:05:14 +0800

    prettify 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <q-layout>
+    <q-layout style="">
       <div class="layout-padding text-center">
-        <p class="caption"><h2><strong>Oops! Looks like you lost your way.</strong></h2></p>
+        <p class="caption"><h3><strong>Oops! Looks like you lost your way.</strong></h3></p>
         <router-link to="/">
+          <img class="responsive" style="max-width: 16rem" src="/static/img/duck.png" />
           <div class="layout-padding">
             <q-btn icon-right="child care" big v-ripple color="primary">
               Back to Quacking
